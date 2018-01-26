e9b8765c3d5c2e48bb21312ad04e64a877a2e1f0
Ryan Dam <31661538+rdam1@users.noreply.github.com>
2018-01-26 09:17:53 -0500

    Add files via upload

---
+++ challenges/colouring/sunny snowy day.js
@@ -0,0 +1,14 @@
+// The ground
+background(76, 205, 219);
+fill(141, 255, 99);
+rect(0, 300, 400, 100);  
+
+// The sun
+fill(255, 238, 0);
+ellipse(80, 64, 100, 100);  
+
+// The snowman
+fill(255, 255, 255);
+ellipse(200, 300, 150, 150);
+ellipse(200, 200, 100, 100);
+ellipse(200, 120, 75, 75);
